3138124218a4184c8f21f7b56c2529283c1142af
susan <m13560492177@163.com>
2022-01-10 19:39:48 +0800

    component icons build

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+var config='https://api.dg.zykjedu.cn/zhxy.files/Asset/'  // 组件my-icons
 Page({
   data:{
     // 组件my-switchBar
@@ -5,7 +6,55 @@ Page({
      // 组件my-switchBarThree
     switch:2, //0  1 2
       // 组件my-search
-    searchValue:null
+    searchValue:null,
+    // 组件my-icons
+    iconlist:[{
+      name: "考勤",
+      url: "/pages/attendance/attendance",
+      imgUrl:config+"icon-calendar.png"
+    }, {
+      name: "审批",
+      url: "/pages/approval/approval",
+      imgUrl: config+"icon-person.png"
+    }, {
+      name: "通知",
+      url: "/pages/noticeManage/noticeManage",
+      imgUrl: config+"icon-notice.png"
+    }, {
+      name: "借阅",
+      url: "/pages/Home/Page/Book/Index",
+      imgUrl: config+"icon-book.png"
+    }, {
+      name: "报修",
+      url: "/pages/fix/fix",
+      imgUrl: config+"icon-fix.png"
+    }],
+      // 组件my-icons2
+      iconlist2:[{
+        name: "考勤",
+        url: "/pages/attendance/attendance",
+        imgUrl:config+"icon-calendar.png"
+      }, {
+        name: "审批",
+        url: "/pages/approval/approval",
+        imgUrl: config+"icon-person.png"
+      }, {
+        name: "通知",
+        url: "/pages/noticeManage/noticeManage",
+        imgUrl: config+"icon-notice.png"
+      }, {
+        name: "借阅",
+        url: "/pages/Home/Page/Book/Index",
+        imgUrl: config+"icon-book.png"
+      }, {
+        name: "报修",
+        url: "/pages/fix/fix",
+        imgUrl: config+"icon-fix.png"
+      }, {
+        name: "报修",
+        url: "/pages/fix/fix",
+        imgUrl: config+"icon-fix.png"
+      }]
     //其他
   },
   // 子组件my-switchBar触发父组件
